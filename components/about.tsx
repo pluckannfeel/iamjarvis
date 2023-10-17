be851a4a3aed53a7658f7888209e93d993e5e4ee
Jarvis <jarvis@tesutonoAir.lan>
2023-10-17 19:36:42 +0900

    fix paragraph on about

--- components/about.tsx
+++ components/about.tsx
@@ -21,7 +21,7 @@ export default function About() {
       <p>
         I am a developer with passion and optimism,{" "}
         <span className="font-medium">desire to meet success and happiness in life.</span> I
-        specialize in full-stack development, with a core stack that includes
+        specialize in full-stack development, with a core stack that includes 
         <span className="font-medium">
           React (JavaScript, TypeScript), Python (FastAPI), and PostgreSQL.
         </span>
@@ -31,7 +31,7 @@ export default function About() {
           {" "}
           Amazon Web Services and Google Cloud Platform.
         </span>
-        In addition, I have a strong background in{" "}
+        Also, I have a strong background in{" "}
         <span className="font-medium">
           data analysis, including ETL data pipelines and data automation used
           for graphs and reports.{" "}
@@ -48,7 +48,7 @@ export default function About() {
         <span className="font-medium">
           new tools in technology such as building mobile apps in React Native
         </span>
-        . I am also also passionate about wonderful scenery and photography.
+        . I am also passionate about wonderful scenery and photography.
         Lastly, I want to learn how to swim.
       </p>
     </motion.section>
